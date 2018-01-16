2677b0a02bfe6b8925078f43313f00eddbf68973
Daddy <Daddy@daddybuk>
2018-01-16 10:48:59 +0300

    OVO начало

--- module/Application/src/Service/PostManager.php
+++ module/Application/src/Service/PostManager.php
@@ -12,7 +12,8 @@ use Zend\Mail\Message;
 use Zend\Mime\Message as MimeMessage;
 use Zend\Mime\Mime;
 use Zend\Mime\Part as MimePart;
-use Zend\Mail\Transport\Sendmail as SendmailTransport;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
 /**
  * Description of PostManager
  *
@@ -33,6 +34,15 @@ class PostManager {
     
     public function send($options)
     {
+        
+        $breaks = array("<br />","<br>","<br/>");  
+        $text = strip_tags(str_ireplace($breaks, PHP_EOL, $options['body']));
+        
+        $text = new MimePart($text);
+        $text->type = Mime::TYPE_TEXT;
+        $text->charset = 'utf-8';
+        $text->encoding = Mime::ENCODING_QUOTEDPRINTABLE;        
+    
         $html = new MimePart($options['body']);
         $html->type = Mime::TYPE_HTML;
         $html->charset = 'utf-8';
@@ -45,14 +55,26 @@ class PostManager {
         $message->setSubject($options['subject']);
         
         $body = new MimeMessage();
-        $body->setParts([$html]);
+        $body->setParts([$text, $html]);
         
         $message->setBody($body);
         
         $contentTypeHeader = $message->getHeaders()->get('Content-Type');
         $contentTypeHeader->setType('multipart/alternative');
 
-        $transport = new SendmailTransport();
+        // Setup SMTP transport using LOGIN authentication
+        $transport = new SmtpTransport();
+        $options   = new SmtpOptions([
+            'name'              => 'localhost.localdomain',
+            'host'              => '127.0.0.1',
+//            'connection_class'  => 'login',
+//            'connection_config' => [
+//                'username' => 'user',
+//                'password' => 'pass',
+//            ],
+        ]);
+        $transport->setOptions($options);
         $transport->send($message);
+
     }
 }
