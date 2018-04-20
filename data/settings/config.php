ec95cf491a826651cea8907f7f12716ac9dd8801
xgavr <dsumin@inbox.ru>
2018-04-20 07:33:22 +0300

    Update config.php

--- data/settings/config.php
+++ data/settings/config.php
@@ -4,7 +4,7 @@ return array(
         'defaultTax' => '1',
     ),
     'admin' => array(
-        'sms_ru_api_id' => '066df1aa-8aae-cba4-51f9-83812a6c9d7f',
-        'sms_ru_url' => 'http://sms.ru/sms/send',
+        'sms_ru_api_id' => '',
+        'sms_ru_url' => '',
     ),
 );
