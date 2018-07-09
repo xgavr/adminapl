83293b25a87341960b3978dc97e49da61cb0f9fe
Daddy <Daddy@daddybuk>
2018-07-10 00:27:36 +0300

    OVO начало

--- module/Application/src/Service/ParseManager.php
+++ module/Application/src/Service/ParseManager.php
@@ -115,6 +115,8 @@ class ParseManager {
         
             $resultParse = [];
             foreach ($spl as $priceDescription){
+                var_dump($spl[$priceDescription]);
+                var_dump(count($resultParse));
                 if (count($spl[$priceDescription]) > count($resultParse) - 1){
                     $resultParse = $spl[$priceDescription];
                     $resultParse['priceDescription'] = $priceDescription;
