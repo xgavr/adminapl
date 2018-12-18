e75c3c0147f05dcb6c8340145196bfc79522c95e
Daddy <Daddy@daddybuk>
2018-12-18 11:56:53 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Controller/GoodsController.php
+++ module/Application/src/Controller/GoodsController.php
@@ -175,7 +175,7 @@ class GoodsController extends AbstractActionController
     public function contentAction()
     {
         	        
-        ini_set('memory_size', '512M');
+        ini_set('memory_limit', '512M');
         
         $q = $this->params()->fromQuery('search');
         $offset = $this->params()->fromQuery('offset');
