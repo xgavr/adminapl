525b09c33202be53f60beb942a01d611c11c21b0
Daddy <Daddy@daddybuk>
2018-12-26 08:59:05 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Service/ExternalManager.php
+++ module/Application/src/Service/ExternalManager.php
@@ -54,7 +54,7 @@ class ExternalManager
     public function autoDb($action, $params = null)
     {
         switch($action){
-            case 'version': $result = $this->autoDbManager->getPegasusVersionInfo2(); break;
+            case 'version': $result = $this->autoDbManager->getManufacturers(); break;
             default: break;
         }
         
