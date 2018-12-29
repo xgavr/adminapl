a27f8285455cd69e2e607ebb6ef08d1358334d64
Daddy <Daddy@daddybuk>
2018-12-29 11:11:15 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Service/GoodsManager.php
+++ module/Application/src/Service/GoodsManager.php
@@ -198,7 +198,7 @@ class GoodsManager
             foreach (new \DirectoryIterator($folderImage) as $fileInfo) {
                 if ($fileInfo->isDot()) continue;
                 if ($fileInfo->isFile()){
-                    $images[basename($folderImage)] = $fileInfo->getRealPath();                            
+                    $images[basename($folderImage)] = $folderImage.'/'.$fileInfo->getFilename();                            
                 }
                 if ($fileInfo->isDir()){
                     $this->imagesFromFolder($fileInfo->getFilename(), $images);                    
