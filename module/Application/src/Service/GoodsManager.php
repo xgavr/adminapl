3605f574c0b203d9370eae5d74129e6fc1b6e97b
Daddy <Daddy@daddybuk>
2018-12-29 11:28:47 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Service/GoodsManager.php
+++ module/Application/src/Service/GoodsManager.php
@@ -192,7 +192,7 @@ class GoodsManager
             $images = [];
         }    
 
-        var_dump($folderImage);
+        var_dump($images);
         if (is_dir($folderImage)){
             foreach (new \DirectoryIterator($folderImage) as $fileInfo) {
                 if ($fileInfo->isDot()) continue;
