cfe4f0d7f6da9b4fbad471856fab1d5e19197516
Daddy <Daddy@daddybuk>
2019-05-11 18:14:26 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Repository/RawRepository.php
+++ module/Application/src/Repository/RawRepository.php
@@ -85,10 +85,10 @@ class RawRepository extends EntityRepository
         $entityManager = $this->getEntityManager();
 
         $queryBuilder = $entityManager->createQueryBuilder();
-        $queryBuilder->select('r.id, rr.dateCreated, r.article, c.code, c.id as codeId, r.producer, identity(r.unknownProducer) as producerId, r.goodname, r.rest, r.price')
+        $queryBuilder->select('r.id, r.article, identity(r.code) as codeId, r.producer, identity(r.unknownProducer) as producerId, r.goodname, r.rest, r.price')
             ->from(Rawprice::class, 'r')
-            ->join('r.code', 'c') 
-            ->join('r.raw', 'rr')
+//            ->join('r.code', 'c') 
+//            ->join('r.raw', 'rr')
             //->join('rr.supplier', 's')    
             ->where('r.unknownProducer = ?1')    
             ->setParameter('1', $unknownProducer->getId())
