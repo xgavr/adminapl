c0d3f90d3bfdf7867a26eaabe724eac675df6207
Daddy <Daddy@daddybuk>
2019-05-21 06:22:54 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Repository/GoodsRepository.php
+++ module/Application/src/Repository/GoodsRepository.php
@@ -706,11 +706,12 @@ class GoodsRepository extends EntityRepository
 
         $queryBuilder = $entityManager->createQueryBuilder();
         $queryBuilder->select('r.id, s.name as supplier, s.id as supplierId, rr.dateCreated, r.article, c.code, c.id as codeId, r.producer, identity(r.unknownProducer) as producerId, r.goodname, r.rest, r.price')
-            ->from(Rawprice::class, 'r')
-            ->join('r.code', 'c') 
+            ->from(Goods::class, 'g')
+            ->join('g.articles', 'c') 
+            ->join('c.rawprice', 'r') 
             ->join('r.raw', 'rr')
             ->join('rr.supplier', 's')    
-            ->where('r.good = ?1')    
+            ->where('g.good = ?1')    
             ->setParameter('1', $good->getId())
             ;
         
