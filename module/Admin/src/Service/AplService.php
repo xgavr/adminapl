727b004b91826d292bbd9d6e5e0c1090fcbece6b
Daddy <Daddy@daddybuk>
2019-06-07 20:07:41 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Admin/src/Service/AplService.php
+++ module/Admin/src/Service/AplService.php
@@ -1024,7 +1024,7 @@ class AplService {
                     'sf'        => $oem->getSourceAsString(),
                 ]; 
             }
-            var_dump($post); exit;
+//            var_dump($post); exit;
             $client = new Client();
             $client->setUri($url);
             $client->setMethod('POST');
