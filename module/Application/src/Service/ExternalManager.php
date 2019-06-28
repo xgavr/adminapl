9f11dc2c9ff71d3daf6f0a299a2da6ebf2bd436e
Daddy <Daddy@daddybuk>
2019-06-28 10:14:35 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Service/ExternalManager.php
+++ module/Application/src/Service/ExternalManager.php
@@ -791,7 +791,7 @@ class ExternalManager
                                                 ]);
                                     }
                                     
-                                    $this->updateGoodCar($good, $$vehicleDetails['modId'], $vehicleDetails, $addFlag);
+                                    $this->updateGoodCar($good, $vehicleDetails['modId'], $vehicleDetails, $addFlag);
                                 }    
                             }
                         }
