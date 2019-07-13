26f28745f999010ba3460b1a1cc8b5d3bc6fd9bd
Daddy <Daddy@daddybuk>
2019-07-13 19:50:42 +0300

    Merge origin/master a.php 	module/Bankapi/view/bankapi/index/index.phtml

--- module/Application/src/Service/GoodsManager.php
+++ module/Application/src/Service/GoodsManager.php
@@ -594,7 +594,7 @@ class GoodsManager
      */
     public function compareGoodsRawprice()
     {
-        ini_set('memory_limit', '2048M');
+        ini_set('memory_limit', '4096M');
         set_time_limit(900);
         $startTime = time();
 
