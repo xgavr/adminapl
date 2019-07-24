3cc9dc8e7c7e0ceaa7d2062124b545864af846e3
Daddy <Daddy@daddybuk>
2019-07-24 14:08:19 +0300

    Merge origin/master
    Conflicts:
    	module/Admin/src/Service/AplService.php

--- module/Admin/src/Service/AplService.php
+++ module/Admin/src/Service/AplService.php
@@ -1814,7 +1814,7 @@ class AplService {
                 ]; 
             }
             
-            var_dump($post); exit;
+//            var_dump($post); exit;
             $client = new Client();
             $client->setUri($url);
             $client->setMethod('POST');
