7f7bed6722ed27e755d0ec00e60c07c1e834f485
Daddy <Daddy@daddybuk>
2019-08-12 07:45:01 +0300

    Merge origin/master
    Conflicts:
    	module/Admin/src/Service/AplService.php

--- module/Application/src/Repository/OemRepository.php
+++ module/Application/src/Repository/OemRepository.php
@@ -433,7 +433,7 @@ class OemRepository  extends EntityRepository{
                 ->setParameter('2', $good->getId())
                 ->setParameter('3', $oe)
                 ;
-
+                var_dump($queryBuilder->getQuery()->getSQL()); exit;
         $iterable = $queryBuilder->getQuery()->iterate();
 
         foreach($iterable as $item){
