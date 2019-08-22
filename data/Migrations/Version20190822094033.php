ee192d98c56b6627d0b429f567b4d45022667bba
Daddy <Daddy@daddybuk>
2019-08-22 13:25:34 +0300

    Merge origin/master
    Conflicts:
    	module/Admin/src/Service/AplService.php

--- data/Migrations/Version20190822094033.php
+++ data/Migrations/Version20190822094033.php
@@ -15,20 +15,26 @@ final class Version20190822094033 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $table = $schema->getTable('car');
         $table->addColumn('good_count', 'integer', ['notnull' => true, 'default' => 0]);
+        $table->addIndex(['good_count'], 'goodCount_indx');
         $table = $schema->getTable('model');
         $table->addColumn('good_count', 'integer', ['notnull' => true, 'default' => 0]);
+        $table->addIndex(['good_count'], 'goodCount_indx');
         $table = $schema->getTable('make');
         $table->addColumn('good_count', 'integer', ['notnull' => true, 'default' => 0]);
+        $table->addIndex(['good_count'], 'goodCount_indx');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $table = $schema->getTable('car');
+        $table->dropIndex('goodCount_indx');
         $table->dropColumn('good_count');
         $table = $schema->getTable('model');
+        $table->dropIndex('goodCount_indx');
         $table->dropColumn('good_count');
         $table = $schema->getTable('make');
+        $table->dropIndex('goodCount_indx');
         $table->dropColumn('good_count');
 
     }
