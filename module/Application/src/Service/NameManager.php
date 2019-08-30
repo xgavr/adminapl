bbd80480821d976199cbbf7c6cd88acbedd65d71
Sumin <Sumin@Server>
2019-08-30 17:16:01 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Service/NameManager.php
+++ module/Application/src/Service/NameManager.php
@@ -948,10 +948,8 @@ class NameManager
             }    
         }    
         
-        $str = iconv('UTF-8', 'UTF-8//IGNORE', $str);
-//        $str = iconv('UTF-16LE', 'UTF-8//IGNORE', $str);
-        $str = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', '', $str);
-        $punktuation = mb_ereg_replace('[A-ZА-ЯЁ0-9 .,/->-+()]', '', mb_strtoupper($str));
+        $stru = iconv('UTF-8', 'UTF-8//IGNORE', $str);
+        $punktuation = mb_ereg_replace('[A-ZА-ЯЁ0-9 .,/->-+()]', '', mb_strtoupper($stru));
         if ($punktuation){
             var_dump($punktuation);
             $result[Token::IS_UNKNOWN] = count(array_unique(str_split($punktuation)));
