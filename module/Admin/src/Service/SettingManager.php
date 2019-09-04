ca43f89727aa7ddcf22b77a6f431e32438f5a715
Daddy <Daddy@daddybuk>
2019-09-04 14:10:14 +0300

    Merge origin/master
    Conflicts:
    	module/Admin/src/Service/AplService.php

--- module/Admin/src/Service/SettingManager.php
+++ module/Admin/src/Service/SettingManager.php
@@ -117,7 +117,7 @@ class SettingManager {
             return TRUE;
         }        
 
-        $lastMod = strtotime($proc->getLastMod()) + 3600;
+        $lastMod = strtotime($proc->getLastMod()) + 3600*3;
         if ($proc->getStatus() == Setting::STATUS_ACTIVE && time() > $lastMod){
             return TRUE;
         }                
