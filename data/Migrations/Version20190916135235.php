15583a7976e213efc68c62e3e101a41eb44ac75c
Sumin <Sumin@Server>
2019-09-17 18:19:02 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- data/Migrations/Version20190916135235.php
+++ data/Migrations/Version20190916135235.php
@@ -13,16 +13,16 @@ final class Version20190916135235 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $table = $schema->getTable('token');
-        $table->addColumn('pseudo_root', 'string', ['notnull' => false]);
-        $table->addColumn('part_of_speech', 'integer', ['notnull' => true, 'default' => -1]);
+//        $table = $schema->getTable('token');
+//        $table->addColumn('pseudo_root', 'string', ['notnull' => false]);
+//        $table->addColumn('part_of_speech', 'integer', ['notnull' => true, 'default' => -1]);
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $table = $schema->getTable('token');
-        $table->dropColumn('pseudo_root');
-        $table->dropColumn('part_of_speech');
+//        $table = $schema->getTable('token');
+//        $table->dropColumn('pseudo_root');
+//        $table->dropColumn('part_of_speech');
     }
 }
