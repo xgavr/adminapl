53cb01c191d474eb608e3d33bef3526fe8257d20
Daddy <Daddy@daddybuk>
2019-10-27 09:45:38 +0300

    Merge origin/master
    Conflicts:
    	module/Admin/src/Service/AplService.php

--- module/Application/src/Service/NameManager.php
+++ module/Application/src/Service/NameManager.php
@@ -1375,7 +1375,7 @@ class NameManager
                             $tf2 = $token->getFrequency()/$gc;
                             $bf = $bigram->getFrequency()/$gc;
 
-                            $pwt = log($bf/($tf1*$tf2));
+                            $pwt = log($bf/($tf1+$tf2));
 
                             if (in_array($bigram->getStatus(), [Bigram::RU_RU, Bigram::RU_EN, Bigram::RU_NUM])){
                                 $result[] = ['bf' => $bf, 'pwt' => $pwt, 'token1' => $preToken, 'token2' => $token, 'bigram' => $bigram];
@@ -1393,7 +1393,7 @@ class NameManager
             $tf2 = $token->getFrequency()/$gc;
             $bf = $token->getFrequency()/$gc;
 
-            $pwt = log($bf/($tf1*$tf2));
+            $pwt = log($bf/($tf1+$tf2));
             $result[] = ['bf' => $bf, 'pwt' => $pwt, 'token1' => $token, 'token2' => $token];            
         }
 //        ksort($result);
