831e0fd5b64128c6188990994e78543507b74ff4
Daddy <Daddy@daddybuk>
2019-11-04 17:43:26 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Repository/BigramRepository.php
+++ module/Application/src/Repository/BigramRepository.php
@@ -327,9 +327,11 @@ class BigramRepository  extends EntityRepository
         if (is_array($params)){
             if (isset($params['q'])){
                 $orX = $queryBuilder->expr()->orX();
-                $orX->add($queryBuilder->expr()->like('b.bilemma', '"%' . $params['q'] . '%"'));
-                $orX->add($queryBuilder->expr()->like('b.correct', '"%' . $params['q'] . '%"'));
-                $queryBuilder->andWhere($orX);
+                $orX->add($queryBuilder->expr()->like('b.bilemma', ':search'));
+                $orX->add($queryBuilder->expr()->like('b.correct', ':search'));
+                $queryBuilder->andWhere($orX)
+                    ->setParameter('search', '%' . $params['q'] . '%')
+                        ;
             }
             if (isset($params['next1'])){
                 $queryBuilder->where('b.bilemma > ?1')
