1f7cac0a8275d29dfbe5b3e90024923555721467
Daddy <Daddy@daddybuk>
2019-11-06 13:34:47 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Service/NameManager.php
+++ module/Application/src/Service/NameManager.php
@@ -1010,27 +1010,27 @@ class NameManager
         
         foreach ($iterable as $row){
             foreach ($row as $rawprice){
-                $good = $rawprice->getGood();
-                if ($good){
-                    $goodTitleStr = $this->goodTitlesIds($good);
-                    $goodTitleStrMd5 = md5($goodTitleStr);
-
-                    if ($goodTitleStr){ 
-                        $goodTitle = $this->entityManager->getRepository(\Application\Entity\GoodTitle::class)
-                                ->findOneBy(['good' => $good->getId(), 'titleMd5' => $goodTitleStrMd5]);
-
-                        if ($goodTitle == null){
-                            
-                            $this->entityManager->getRepository(\Application\Entity\Goods::class)
-                                    ->removeGoodTitles($good);
-                            
-                            $this->addGoodTokenFromGood($good, $avgD);
-
-                            $this->entityManager->getRepository(\Application\Entity\Goods::class)
-                                    ->insertGoodTitle(['good_id' => $good->getId(), 'title' => $goodTitleStr, 'title_md5' => $goodTitleStrMd5]);
-                        }    
-                    }    
-                }    
+//                $good = $rawprice->getGood();
+//                if ($good){
+//                    $goodTitleStr = $this->goodTitlesIds($good);
+//                    $goodTitleStrMd5 = md5($goodTitleStr);
+//
+//                    if ($goodTitleStr){ 
+//                        $goodTitle = $this->entityManager->getRepository(\Application\Entity\GoodTitle::class)
+//                                ->findOneBy(['good' => $good->getId(), 'titleMd5' => $goodTitleStrMd5]);
+//
+//                        if ($goodTitle == null){
+//                            
+//                            $this->entityManager->getRepository(\Application\Entity\Goods::class)
+//                                    ->removeGoodTitles($good);
+//                            
+//                            $this->addGoodTokenFromGood($good, $avgD);
+//
+//                            $this->entityManager->getRepository(\Application\Entity\Goods::class)
+//                                    ->insertGoodTitle(['good_id' => $good->getId(), 'title' => $goodTitleStr, 'title_md5' => $goodTitleStrMd5]);
+//                        }    
+//                    }    
+//                }    
 
                 $this->entityManager->getRepository(Rawprice::class)
                         ->updateRawpriceField($rawprice->getId(), ['status_token' => Rawprice::TOKEN_GOOD_PARSED]);
