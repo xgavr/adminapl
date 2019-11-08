94bfe1a0016e4446d56158015f33ef1e9bd45aac
Sumin <Sumin@Server>
2019-11-08 17:39:08 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Controller/NameController.php
+++ module/Application/src/Controller/NameController.php
@@ -921,7 +921,7 @@ class NameController extends AbstractActionController
         
         return new JsonModel([
             'result' => 'ok-reload',
-            'message' => array_pop($associator->apriori()),
+            'message' => $associator->apriori(),
         ]);          
     }
     
