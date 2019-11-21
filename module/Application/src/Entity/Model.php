2adf0919fc6397a867c20872e3c9952c3ed04782
Sumin <Sumin@Server>
2019-11-21 18:05:03 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Entity/Model.php
+++ module/Application/src/Entity/Model.php
@@ -164,7 +164,7 @@ class Model {
         if ($this->fullName){
             return $this->fullName;
         }
-        return ucwords(strtolower($this->name), '-+ ');
+        return $this->name;
     }
     
     public function setFullName($fullName) 
