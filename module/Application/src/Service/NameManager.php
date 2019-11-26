3fe949e4bc621c06b29a9fd8f9e584adb326b5e6
Sumin <Sumin@Server>
2019-11-26 16:42:57 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Service/NameManager.php
+++ module/Application/src/Service/NameManager.php
@@ -1974,7 +1974,7 @@ class NameManager
         
         if ($flag){
             $this->entityManager->getRepository(Goods::class)
-                    ->updateGood($good, ['name' => $result['bestname']]);
+                    ->updateGood($good, ['name' => $result['bestName']]);
             return;
         }
 
