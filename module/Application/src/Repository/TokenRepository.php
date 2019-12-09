018d676876f31cdbcbe4d9c311aea0f5701f7d11
Sumin <Sumin@Server>
2019-12-09 18:28:54 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Repository/TokenRepository.php
+++ module/Application/src/Repository/TokenRepository.php
@@ -340,8 +340,8 @@ class TokenRepository  extends EntityRepository
                         ->setParameter('3', $levels[0]);
                     if (isset($levels[1])){
                         if ($level[1] >= $levels[0]){
-                            $queryBuilder->andWhere('tg.goodCount < ?5')
-                                ->setParameter('5', $levels[1]);
+                            $queryBuilder->andWhere('tg.goodCount <= ?4')
+                                ->setParameter('4', $levels[1]);
                         }
                     }
                 }                                
