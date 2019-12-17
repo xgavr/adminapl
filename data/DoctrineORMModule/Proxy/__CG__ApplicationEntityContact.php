44c6f1741777a0a5020b201f50a31aac1d374100
Daddy <Daddy@daddybuk>
2019-12-17 11:05:03 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- data/DoctrineORMModule/Proxy/__CG__ApplicationEntityContact.php
+++ data/DoctrineORMModule/Proxy/__CG__ApplicationEntityContact.php
@@ -12,14 +12,14 @@ class Contact extends \Application\Entity\Contact implements \Doctrine\ORM\Proxy
      *      three parameters, being respectively the proxy object to be initialized, the method that triggered the
      *      initialization process and an array of ordered parameters that were passed to that method.
      *
-     * @see \Doctrine\Common\Persistence\Proxy::__setInitializer
+     * @see \Doctrine\Common\Proxy\Proxy::__setInitializer
      */
     public $__initializer__;
 
     /**
      * @var \Closure the callback responsible of loading properties that need to be copied in the cloned object
      *
-     * @see \Doctrine\Common\Persistence\Proxy::__setCloner
+     * @see \Doctrine\Common\Proxy\Proxy::__setCloner
      */
     public $__cloner__;
 
@@ -34,7 +34,7 @@ class Contact extends \Application\Entity\Contact implements \Doctrine\ORM\Proxy
      * @var array properties to be lazy loaded, with keys being the property
      *            names and values being their default values
      *
-     * @see \Doctrine\Common\Persistence\Proxy::__getLazyProperties
+     * @see \Doctrine\Common\Proxy\Proxy::__getLazyProperties
      */
     public static $lazyPropertiesDefaults = [];
 
@@ -648,6 +648,17 @@ class Contact extends \Application\Entity\Contact implements \Doctrine\ORM\Proxy
         $this->__initializer__ && $this->__initializer__->__invoke($this, 'removeLegalAssociation', [$legal]);
 
         return parent::removeLegalAssociation($legal);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAsText()
+    {
+
+        $this->__initializer__ && $this->__initializer__->__invoke($this, 'getAsText', []);
+
+        return parent::getAsText();
     }
 
 }
