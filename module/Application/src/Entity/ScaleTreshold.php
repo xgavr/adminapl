68f6144bf09fa5c254ff6c94e23632f32be117e2
Daddy <Daddy@daddybuk>
2020-01-14 08:37:31 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Entity/ScaleTreshold.php
+++ module/Application/src/Entity/ScaleTreshold.php
@@ -147,9 +147,9 @@ class ScaleTreshold
         $result = [];
         $col = 0;
         $minRetailPrice = ceil($price + $price*self::MIN_RATE/100);
-        $priceTreshold = ($retailPrice - $minRetailPrice)/(self::PRICE_COL_COUNT - 1);
+        $priceTreshold = ($retailPrice - $price)/(self::PRICE_COL_COUNT);
         
-        while ($col <= self::PRICE_COL_COUNT-1){
+        while ($col <= self::PRICE_COL_COUNT){
             $result[$col]['price'] = round($retailPrice - $col*$priceTreshold, self::DEFAULT_ROUNDING);
             if ($result[$col]['price'] < $minRetailPrice){
                 $result[$col]['price'] = $minRetailPrice;
@@ -161,12 +161,6 @@ class ScaleTreshold
             }    
             $col++;
         }
-        $result[self::PRICE_COL_COUNT]['price'] = $minRetailPrice;
-        if ($price){
-            $result[self::PRICE_COL_COUNT]['percent'] = ($minRetailPrice - $price)*100/$price;
-        } else {
-            $result[self::PRICE_COL_COUNT]['percent'] = 0;
-        }    
         
         return $result;
         
