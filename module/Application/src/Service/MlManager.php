bf0b2868769873aa4efeb38876f2b2f2e68c6d01
Daddy <Daddy@daddybuk>
2020-01-16 11:48:32 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Service/MlManager.php
+++ module/Application/src/Service/MlManager.php
@@ -412,7 +412,8 @@ class MlManager
                             ->findBigram($preWord, $word);
                     if ($bigram && $preToken->getFrequency() > 0 && $token->getFrequency() > 0){
                         if (in_array($bigram->getStatus(), [Bigram::RU_RU, Bigram::RU_EN, Bigram::RU_NUM])){
-                            $pmi = log($bigram->getGf()*$tgc/($preToken->getGf()*$token->getGf() + 0.05))*$bigram->getIdf();
+                            $pmi = log($bigram->getGf()*$tgc/($preToken->getGf()*$token->getGf() + 0.05))*
+                                    ($preToken>getIdf() + $token->getIdf() - $bigram->getIdf());
                             if ($pmi < 0 
 //                                    || $bigram->getFlag() != Bigram::WHITE_LIST
 //                                    || $bigram->getFrequency() < 10
@@ -433,7 +434,8 @@ class MlManager
 
             if ($bigram){
                 if (in_array($bigram->getStatus(), [Bigram::RU_RU, Bigram::RU_EN, Bigram::RU_NUM])){
-                    $pmi = log(($bigram->getGf()*$tgc)/($token->getGf()*2 + 0.05))*$bigram->getIdf();
+                    $pmi = log(($bigram->getGf()*$tgc)/($token->getGf()*2 + 0.05))*
+                            ($token->getIdf()*2 - $bigram->getIdf());
                     if ($pmi < 0 
 //                            || $bigram->getFlag() != Bigram::WHITE_LIST
 //                            || $bigram->getFrequency() < 10
