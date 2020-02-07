cb5d4b1b0b06626cf804939dc3b0e5b32bae4d0c
Daddy <Daddy@daddybuk>
2020-02-07 11:19:01 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Repository/FpTreeRepository.php
+++ module/Application/src/Repository/FpTreeRepository.php
@@ -484,10 +484,10 @@ class FpTreeRepository  extends EntityRepository{
             ;  
         
         $tokens = $queryBuilder->getQuery()->getResult();
-        var_dump(count($tokens)); exit;
+
         foreach ($tokens as $row){
             $token = $this->getEntityManager()->getRepository(Token::class)
-                    ->findOneById(row['tokenId']);
+                    ->findOneById($row['tokenId']);
             if ($token){
                 $this->updateFpGroup($token);
             }
