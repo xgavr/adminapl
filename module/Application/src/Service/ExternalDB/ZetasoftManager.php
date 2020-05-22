255a7359a34cc274612f97e36cbe940ea6737617
Daddy <Daddy@daddybuk>
2020-05-22 09:25:26 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Service/ExternalDB/ZetasoftManager.php
+++ module/Application/src/Service/ExternalDB/ZetasoftManager.php
@@ -728,7 +728,8 @@ class ZetasoftManager
 
                 if (isset($articleInfo['images'])){
                     foreach($articleInfo['images'] as $document){
-                        $uri = $this->getDocImageUri($document['url800']);
+//                        $uri = $this->getDocImageUri($document['url800']);
+                        $uri = trim($document['url800']);
 //                        var_dump($uri); exit;
                         $this->entityManager->getRepository(Images::class)
                                 ->saveImageGood($good, $uri, $document['fileName'], Images::STATUS_TD, $similar);
