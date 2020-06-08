083c263a4e1ac2b84a27ec324f20c35e5b58b3f2
Daddy <Daddy@daddybuk>
2020-06-08 18:14:40 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Admin/src/Service/AplService.php
+++ module/Admin/src/Service/AplService.php
@@ -1305,7 +1305,7 @@ class AplService {
             $ok = $result = false;
             try{
                 $response = $client->send();
-                var_dump($response->getBody()); exit;
+//                var_dump($response->getBody()); exit;
                 if ($response->isOk()) {
                     $ok = $result = true;
                 }
