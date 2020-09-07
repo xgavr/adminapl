7066513814e9156210d6d77c45884461db16de74
Daddy <Daddy@daddybuk>
2020-09-07 11:48:26 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Service/ExternalDB/ZetasoftManager.php
+++ module/Application/src/Service/ExternalDB/ZetasoftManager.php
@@ -676,7 +676,7 @@ class ZetasoftManager
             $models = [];
             if (is_array($manufacturers)){
                 if (isset($manufacturers['data'])){
-                    var_dump(count($manufacturers['data'])); exit;
+//                    var_dump(count($manufacturers['data'])); exit;
                     if (count($manufacturers['data']) < 20){
                         foreach ($manufacturers['data'] as $manufacturer){
                             $models[$manufacturer['id']] = $this->getModels($article['vendorCode'], $article['vendorName'], $manufacturer['id']);
