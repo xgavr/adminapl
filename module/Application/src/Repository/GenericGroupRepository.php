c01bb8663f4d7b7cc7b6bf26750dada76fb39227
Daddy <Daddy@daddybuk>
2020-11-08 07:52:00 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Repository/GenericGroupRepository.php
+++ module/Application/src/Repository/GenericGroupRepository.php
@@ -209,7 +209,7 @@ class GenericGroupRepository extends EntityRepository{
             $counts[] = $group['goodCount'];
         }
         if (count($counts)){
-            $mean = Mean::arithmetic($counts);
+            $mean = Mean::median($counts);
             var_dump($mean);
             $dispersion = StandardDeviation::population($counts, count($counts)>1);
             var_dump($dispersion); 
