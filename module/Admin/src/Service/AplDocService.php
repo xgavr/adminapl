38090a21b64b37eadc98043eea6a171bfa3ac88e
Daddy <Daddy@daddybuk>
2020-11-25 10:31:47 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Admin/src/Service/AplDocService.php
+++ module/Admin/src/Service/AplDocService.php
@@ -368,7 +368,7 @@ class AplDocService {
                 ->findOneByAplId($data['id']);
         if ($ptu){
             $this->ptuManager->updatePtu($ptu, $dataPtu);
-            $this->entityManager->refresh($ptu);
+            $this->entityManager->clear($ptu);
             $this->ptuManager->removePtuGood($ptu); 
         } else {        
             $ptu = $this->ptuManager->addPtu($dataPtu);
