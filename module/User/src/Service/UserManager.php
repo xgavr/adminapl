2b8ec6d1a9746fc6f88900235f045932511f5282
Daddy <Daddy@daddybuk>
2021-05-25 12:32:38 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/User/src/Service/UserManager.php
+++ module/User/src/Service/UserManager.php
@@ -82,7 +82,10 @@ class UserManager
         
         $user->setStatus($data['status']);
         $user->setAplId($data['aplId']);
-        $user->setBirthday($data['birthday']);
+        $user->setBirthday(null);
+        if (!empty($data['birthday'])){
+            $user->setBirthday($data['birthday']);
+        }    
         
         $currentDate = date('Y-m-d H:i:s');
         $user->setDateCreated($currentDate);        
@@ -128,7 +131,10 @@ class UserManager
         $user->setFullName($data['full_name']);        
         $user->setStatus($data['status']);
         $user->setAplId($data['aplId']);
-        $user->setBirthday($data['birthday']);
+        $user->setBirthday(null);
+        if (!empty($data['birthday'])){
+            $user->setBirthday($data['birthday']);
+        }    
         
         // Assign roles to user.
         $this->assignRoles($user, $data['roles']);
