61d15e5a0fba5eaf8731a9ee5524b8e9d33108da
Daddy <Daddy@daddybuk>
2021-06-30 12:24:00 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/NameManager.php

--- module/Application/src/Service/ContactCarManager.php
+++ module/Application/src/Service/ContactCarManager.php
@@ -88,6 +88,53 @@ class ContactCarManager
     }
     
     /**
+     * Добавить машину контакта
+     * @param Contact $contact
+     * @param array $data
+     * @return ContactCar
+     */
+    public function ins($contact, $data)
+    {
+        $upd = [
+            'contact_id' => $contact->getId(),
+            'apl_id' => (!empty($data['aplId'])) ? $data['aplId'] : null,
+            'comment' => (!empty($data['comment'])) ? $data['comment'] : null,
+            'vin' =>(!empty($data['vin'])) ? $data['vin'] : null,
+            'vin2' =>(!empty($data['vin2'])) ? $data['vin2'] : null,
+            'status' => (!empty($data['status'])) ? $data['status'] : ContactCar::STATUS_ACTIVE,
+            'yocm' =>(!empty($data['yocm'])) ? $data['yocm'] : null,
+            'wheel' => (!empty($data['wheel'])) ? $data['wheel'] : ContactCar::WHEEL_LEFT,
+            'tm' => (!empty($data['tm'])) ? $data['tm'] : ContactCar::TM_UNKNOWN,
+            'ac' => (!empty($data['ac'])) ? $data['ac'] : ContactCar::AC_UNKNOWN,
+            'md' =>(!empty($data['md'])) ? $data['md'] : null,
+            'ed' =>(!empty($data['ed'])) ? $data['ed'] : null,
+            'ep' =>(!empty($data['ep'])) ? $data['ep'] : null,
+            'make_id' => null,
+            'model_id' => null,
+            'car_id' => null,
+            'date_created' => date('Y-m-d H:i:s'),
+        ];
+
+        if (isset($data['make'])){
+            $upd['make_id'] = $data['make'];
+        }    
+        if (isset($data['model'])){
+            $upd['model_id'] = $data['model'];
+        }    
+        if (isset($data['car'])){
+            $upd['car_id'] = $data['car'];
+        }    
+
+
+        $this->entityManager->getConnection()
+                ->insert('contact_car', $add);
+        $contactCar = $this->entityManager->getRepository(ContactCar::class)
+                ->findOneBy([], ['id'=>'DESC'],1,0);
+        
+        return $contactCar;
+    }
+    
+    /**
      * Обновить машину контакта
      * @param ContactCar $contactCar
      * @param array $data
@@ -133,6 +180,49 @@ class ContactCarManager
         return $contactCar;
     }
     
+    /**
+     * Обновить машину контакта
+     * 
+     * @param ContactCar $contactCar
+     * @param array $data
+     * @return type
+     */
+    public function upd($contactCar, $data)
+    {
+        $upd = [
+            'apl_id' => (!empty($data['aplId'])) ? $data['aplId'] : null,
+            'comment' => (!empty($data['comment'])) ? $data['comment'] : null,
+            'vin' =>(!empty($data['vin'])) ? $data['vin'] : null,
+            'vin2' =>(!empty($data['vin2'])) ? $data['vin2'] : null,
+            'status' => (!empty($data['status'])) ? $data['status'] : ContactCar::STATUS_ACTIVE,
+            'yocm' =>(!empty($data['yocm'])) ? $data['yocm'] : null,
+            'wheel' => (!empty($data['wheel'])) ? $data['wheel'] : ContactCar::WHEEL_LEFT,
+            'tm' => (!empty($data['tm'])) ? $data['tm'] : ContactCar::TM_UNKNOWN,
+            'ac' => (!empty($data['ac'])) ? $data['ac'] : ContactCar::AC_UNKNOWN,
+            'md' =>(!empty($data['md'])) ? $data['md'] : null,
+            'ed' =>(!empty($data['ed'])) ? $data['ed'] : null,
+            'ep' =>(!empty($data['ep'])) ? $data['ep'] : null,
+            'make_id' => null,
+            'model_id' => null,
+            'car_id' => null,
+        ];
+
+        if (isset($data['make'])){
+            $upd['make_id'] = $data['make'];
+        }    
+        if (isset($data['model'])){
+            $upd['model_id'] = $data['model'];
+        }    
+        if (isset($data['car'])){
+            $upd['car_id'] = $data['car'];
+        }    
+        
+        $this->entityManager->getConnection()
+                ->update('contact_car', $upd, ['id' => $contactCar->getId()]);
+        
+        return $contactCar;
+    }
+
     /**
      * Удалить машину контакта
      * @param ContactCar $contactCar
