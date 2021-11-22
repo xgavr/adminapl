bb436c384aada69d15e77c33d7df5905f296ebb8
Sumin <Sumin@Server>
2021-11-22 16:52:46 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Repository/MarketRepository.php
+++ module/Application/src/Repository/MarketRepository.php
@@ -171,6 +171,7 @@ class MarketRepository extends EntityRepository{
             $queryBuilder->setFirstResult($offset);
         }
         
+        var_dump($queryBuilder->getQuery()->getSQL());
         $query = $queryBuilder->getQuery();
         
         return $query;
