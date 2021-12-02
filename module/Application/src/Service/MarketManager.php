5417ecdbe7208a788c6ebc35e60abaa50511a445
Sumin <Sumin@Server>
2021-12-02 17:49:16 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Application/src/Service/MarketManager.php
+++ module/Application/src/Service/MarketManager.php
@@ -320,13 +320,12 @@ class MarketManager
     {
         $imageList = [];
         if (!empty($market->getImageCount())){            
-            if ($market->getGoodSetting() == MarketPriceSetting::IMAGE_ALL){
-                $images = $this->entityManager->getRepository(Images::class)
-                        ->arrayGoodImages($good['id'], ['limit' => $market->getImageCountOrNull()]);
-            }
             if ($market->getGoodSetting() == MarketPriceSetting::IMAGE_MATH){
                 $images = $this->entityManager->getRepository(Images::class)
                         ->arrayGoodImages($good['id'], ['similar' => Images::SIMILAR_MATCH, 'limit' => $market->getImageCountOrNull()]);
+            } else {
+                $images = $this->entityManager->getRepository(Images::class)
+                        ->arrayGoodImages($good['id'], ['limit' => $market->getImageCountOrNull()]);                
             }
             if (!empty($images)){
                 foreach ($images as $image){
