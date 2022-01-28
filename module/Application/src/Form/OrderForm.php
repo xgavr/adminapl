8d2e866f7ee423e480ef3a5699fef47518830fc9
Daddy <Daddy@daddybuk>
2022-01-28 14:27:02 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/ContactCarManager.php

---
+++ module/Application/src/Form/OrderForm.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Application\Form;
+
+use Laminas\Form\Form;
+use Laminas\InputFilter\InputFilter;
+use Application\Entity\Client;
+use Application\Entity\Order;
+
+/**
+ * Description of Order
+ *
+ * @author Daddy
+ */
+class OrderForm extends Form
+{
+    
+    protected $objectManager;
+
+    protected $entityManager;
+        
+    /**
+     * Конструктор.     
+     */
+    public function __construct($entityManager)
+    {
+        // Определяем имя формы.
+        parent::__construct('order-form');
+     
+        $this->entityManager = $entityManager;
+        // Задает для этой формы метод POST.
+        $this->setAttribute('method', 'post');
+                
+        $this->addElements();
+        $this->addInputFilter();         
+    }
+    
+    /**
+    * Этот метод добавляет элементы к форме (поля ввода и кнопку отправки формы).
+    */
+    protected function addElements() 
+    {
+                
+        // Добавляем поле "name"
+        $this->add([           
+            'type'  => 'text',
+            'name' => 'name',
+            'attributes' => [
+                'id' => 'name'
+            ],
+            'options' => [
+                'label' => 'Имя',
+            ],
+        ]);
+        
+        $this->add([           
+            'type'  => 'text',
+            'name' => 'phone',
+            'attributes' => [
+            ],
+            'options' => [
+                'label' => 'Телефон',
+            ],
+        ]);
+        
+        $this->add([           
+            'type'  => 'text',
+            'name' => 'email',
+            'attributes' => [
+                'id' => 'email'
+            ],
+            'options' => [
+                'label' => 'Email',
+            ],
+        ]);
+        
+        $this->add([           
+            'type'  => 'text',
+            'name' => 'email',
+            'attributes' => [
+                'id' => 'email'
+            ],
+            'options' => [
+                'label' => 'Email',
+            ],
+        ]);
+        
+        // Добавляем поле "address"
+        $this->add([           
+            'type'  => 'text',
+            'name' => 'address',
+            'attributes' => [
+                'id' => 'address'
+            ],
+            'options' => [
+                'label' => 'Адрес доставки',
+            ],
+        ]);
+        
+        // Добавляем кнопку отправки формы
+        $this->add([
+            'type'  => 'submit',
+            'name' => 'submit',
+            'attributes' => [                
+                'value' => 'Сохранить',
+                'id' => 'order_submitbutton',
+            ],
+        ]);        
+    }
+    
+   /**
+     * Этот метод создает фильтр входных данных (используется для фильтрации/валидации).
+     */
+    private function addInputFilter() 
+    {
+        
+        $inputFilter = new InputFilter();        
+        $this->setInputFilter($inputFilter);
+        
+        $inputFilter->add([
+                'name'     => 'name',
+                'required' => true,
+                'filters'  => [
+                    ['name' => 'StringTrim'],
+                    ['name' => 'StripTags'],
+                    ['name' => 'StripNewlines'],
+                ],                
+                'validators' => [
+                    [
+                        'name'    => 'StringLength',
+                        'options' => [
+                            'min' => 1,
+                            'max' => 1024
+                        ],
+                    ],
+                ],
+            ]);
+        
+                
+    }    
+    
+    
+    public function setObjectManager(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    public function getObjectManager()
+    {
+        return $this->objectManager;
+    }    
+
+    
+}
