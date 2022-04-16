ed61b36a6961122df6fd2d2987448c27e29f4177
Daddy <Daddy@daddybuk>
2022-04-16 06:51:26 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/ContactCarManager.php

--- module/Application/src/Repository/SupplierRepository.php
+++ module/Application/src/Repository/SupplierRepository.php
@@ -299,7 +299,7 @@ class SupplierRepository extends EntityRepository{
             $orX->add($queryBuilder->expr()->eq('gs.supplier', $child->getId()));            
         }
         $queryBuilder->andWhere($orX);
-        
+        var_dump($queryBuilder->getQuery()->getSQL()); exit;
         return $queryBuilder->getQuery()->getOneOrNullResult();
     }
     
