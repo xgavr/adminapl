b53b0c6197b8bd369f369ddf78a8c2bc8a13da92
Sumin <Sumin@Server>
2022-05-13 18:15:07 +0300

    Merge origin/master
    Conflicts:
    	data/log/telegramm.log

--- module/Admin/src/Service/AplDocService.php
+++ module/Admin/src/Service/AplDocService.php
@@ -723,7 +723,7 @@ class AplDocService {
             }
             $post['tp'] = $so;
             
-//            var_dump($post); exit;
+            var_dump($post); exit;
             $client = new Client();
             $client->setUri($url);
             $client->setMethod('POST');
