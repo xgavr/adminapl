feae9f9f9530f2ca386e680d3f3c90f352e20dbf
Daddy <Daddy@daddybuk>
2022-07-20 09:49:43 +0300

    Merge origin/master
    Conflicts:
    	module/Application/src/Service/ContactCarManager.php

--- module/Application/src/Repository/OemRepository.php
+++ module/Application/src/Repository/OemRepository.php
@@ -609,7 +609,7 @@ class OemRepository  extends EntityRepository{
      */
     public function removeIntersectOem($good)
     {
-        $bidCount = $this->entityManager->getRepository(Bid::class)
+        $bidCount = $this->getEntityManager()->getRepository(Bid::class)
                 ->count(['good' => $good->getId()]);
         
         if (empty($bidCount)){
