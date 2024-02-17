a4ac6a1360b4026179bcc832bc639ebaa871685a
dsumi <dsumi@hpbook>
2024-02-17 23:14:24 +0300

    nt

--- module/Bank/src/Service/BankManager.php
+++ module/Bank/src/Service/BankManager.php
@@ -192,7 +192,7 @@ class BankManager
         ];
         
         $result = $this->gigaManager->completions($messages);
-        var_dump($result);
+//        var_dump($result);
         if (!empty($result['choices'])){
             foreach ($result['choices'] as $choice){
                 if (!empty($choice['message']['content'])){
