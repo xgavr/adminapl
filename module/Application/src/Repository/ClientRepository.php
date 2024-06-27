1ef73eb12dff81c1203da845f4d99edd77e9b3b8
dsumi <dsumi@hpbook>
2024-06-27 11:29:58 +0300

    nt

--- module/Application/src/Repository/ClientRepository.php
+++ module/Application/src/Repository/ClientRepository.php
@@ -172,7 +172,7 @@ class ClientRepository extends EntityRepository{
             }    
         }    
         if (!empty($params['legal'])){
-            $queryBuilder->join('c.contacts', 'cntl')
+            $queryBuilder->join('c.contacts')
                     ->join('cntl.legals', 'l')
                     ->join('l.contracts', 'contract')
                     ->addSelect('cntl')
@@ -245,6 +245,8 @@ class ClientRepository extends EntityRepository{
             $queryBuilder->join('c.contacts', 'cntl')
                     ->join('cntl.legals', 'l')
                     ->join('l.contracts', 'contract')
+                    ->addSelect('cntl')
+                    ->addSelect('l')
                     ->addSelect('sum(case when contract.balance > 0 then contract.balance else 0 end) as contractBalanceIn')
                     ->addSelect('sum(case when contract.balance < 0 then -contract.balance else 0 end) as contractBalanceOut')
                     ;
