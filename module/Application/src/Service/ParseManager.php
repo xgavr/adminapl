e6736330f60fb7fc6151885df2190d7fec5463c7
dsumi <dsumi@hpbook>
2024-07-25 09:00:41 +0300

    nt

--- module/Application/src/Service/ParseManager.php
+++ module/Application/src/Service/ParseManager.php
@@ -322,6 +322,7 @@ class ParseManager {
         $modelManager = new ModelManager();
         $restoredClassifier = $modelManager->restoreFromFile($filepath);
         $data = $this->compareRaw($raw, $oldRaw);
+        var_dump([$data['strPer'], $data['rowPer']]); exit;
         return $restoredClassifier->predict([$data['strPer'], $data['rowPer']]);        
     }
 
