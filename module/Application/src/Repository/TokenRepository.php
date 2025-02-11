a34aadd5d8611afd5dc19244cc52e1d07c940269
dsumi <dsumi@hpbook>
2025-02-11 23:42:00 +0300

    nt

--- module/Application/src/Repository/TokenRepository.php
+++ module/Application/src/Repository/TokenRepository.php
@@ -330,8 +330,8 @@ class TokenRepository  extends EntityRepository
 //                var_dump($lemms);
                 
                 foreach ($lemms as $lemma){
-                    $queryBuilder->andWhere('tg.lemms like ":lemma"')
-                        ->setParameter('lemma', '%' . $lemma . '%')
+                    $queryBuilder->andWhere('tg.lemms like \'%:lemma%\'')
+                        ->setParameter('lemma', $lemma)
                             ;
                 }
             }
