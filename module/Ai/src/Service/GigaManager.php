decebf5518fe185c1ca9ddb1f7e45737d8bec728
dsumi <dsumi@hpbook>
2025-02-28 10:41:57 +0300

    nt

--- module/Ai/src/Service/GigaManager.php
+++ module/Ai/src/Service/GigaManager.php
@@ -75,8 +75,8 @@ class GigaManager {
         $aiSettings = $this->adminManager->getAiSettings();
         
         if (!empty($aiSettings['gigachat_expires_at']) && !empty($aiSettings['gigachat_access_token'])){
-            var_dump(intval($aiSettings['gigachat_expires_at']), time()+60);
             if (intval($aiSettings['gigachat_expires_at']) > time()+60){
+                var_dump(intval($aiSettings['gigachat_expires_at']), time()+60, intval($aiSettings['gigachat_expires_at']) > time()+60);
                 return $aiSettings['gigachat_access_token'];
             }            
         }
