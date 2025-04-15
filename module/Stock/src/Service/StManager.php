cebdab6acb86ba680480f40defccce1c4a36c966
dsumi <dsumi@hpbook>
2025-04-15 19:16:15 +0300

    nt

--- module/Stock/src/Service/StManager.php
+++ module/Stock/src/Service/StManager.php
@@ -400,7 +400,7 @@ class StManager
 //            'info' => $data['info'],
             'row_no' => $rowNo,
             'take' => StGood::TAKE_NO,
-            'baseKey' => empty($data['baseKey']) ? null:$data['baseKey'],
+            'base_key' => empty($data['baseKey']) ? null:$data['baseKey'],
         ];
         //var_dump($stGood); exit;
         
