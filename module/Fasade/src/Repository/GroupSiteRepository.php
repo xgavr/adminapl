cfcea6cd4c45f9200ee603abd55b4f17d78e89dc
dsumi <dsumi@hpbook>
2025-05-05 05:29:48 +0300

    nt

--- module/Fasade/src/Repository/GroupSiteRepository.php
+++ module/Fasade/src/Repository/GroupSiteRepository.php
@@ -110,7 +110,7 @@ class GroupSiteRepository extends EntityRepository{
      */
     public function updateGroupSiteGoodCounts()
     {
-        ini_set('memory_limit', '1024M');
+        ini_set('memory_limit', '2048M');
         
         $entityManager = $this->getEntityManager();
         $categories = $entityManager->getRepository(GroupSite::class)
