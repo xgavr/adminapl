5b5643aae9d05f43f807c73421ac8b6134f19130
dsumi <dsumi@hpbook>
2025-05-07 10:09:40 +0300

    nt

--- module/Fasade/src/Service/GroupSiteManager.php
+++ module/Fasade/src/Service/GroupSiteManager.php
@@ -128,14 +128,18 @@ class GroupSiteManager
             if (is_numeric($data['groupSite'])){
                 $data['groupSite'] = $this->entityManager->getRepository(GroupSite::class)
                         ->find($data['groupSite']);
-                $groupSite->setSiteGroup($data['groupSite'] ?? null);
-            }
-            $groupSite->setLevel($data['groupSite']->getLevel() + 1);
-            $code = $data['groupSite']->getCode().'-'.$code;
-            $fullName = $data['groupSite']->getFullName().'/'.$data['name'];
+                
+                if ($data['groupSite']){
+                    $groupSite->setSiteGroup($data['groupSite'] ?? null);
 
-            $data['groupSite']->setHasChild(GroupSite::HAS_CHILD);
-            $this->entityManager->persist($data['groupSite']);
+                    $groupSite->setLevel($data['groupSite']->getLevel() + 1);
+                    $code = $data['groupSite']->getCode().'-'.$code;
+                    $fullName = $data['groupSite']->getFullName().'/'.$data['name'];
+
+                    $data['groupSite']->setHasChild(GroupSite::HAS_CHILD);
+                    $this->entityManager->persist($data['groupSite']);
+                }    
+            }
         }
         
         $groupSite->setCode($code);
