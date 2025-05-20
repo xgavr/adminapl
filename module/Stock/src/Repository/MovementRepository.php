718138cab5f270b8e0b8edf9fea767d4dbb3dfaa
dsumi <dsumi@hpbook>
2025-05-20 20:26:56 +0300

    nt

--- module/Stock/src/Repository/MovementRepository.php
+++ module/Stock/src/Repository/MovementRepository.php
@@ -430,7 +430,7 @@ class MovementRepository extends EntityRepository{
         $entityManager = $this->getEntityManager();
 
         $qb = $entityManager->createQueryBuilder();
-        $qb->select('sum(m.quantity) as rSum, sum(abs(m.quantity)*(m.amount-m.baseAmount)) as mSum')
+        $qb->select('sum(m.quantity) as rSum, sum(m.amount-m.baseAmount) as mSum')
                 ->from(Movement::class, 'm')
                 ->where('m.good = ?1')
                 ->andWhere('m.docType = ?2 or m.docType = ?3')
@@ -449,7 +449,7 @@ class MovementRepository extends EntityRepository{
         $result = $qb->getQuery()->getOneOrNullResult();
         //$connection->update('goods', ['movement' => -$result['rSum']],['id' => $good->getId()]);
         
-        return abs(intval($result['mSum']));
+        return intval($result['rSum']*$result['mSum']);
     }      
         
     /**
