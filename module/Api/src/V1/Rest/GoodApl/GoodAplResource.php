ab4e77de2f16d56cec01a544038df4bae3261e2b
dsumi <dsumi@hpbook>
2025-05-30 19:57:49 +0300

    nt

--- module/Api/src/V1/Rest/GoodApl/GoodAplResource.php
+++ module/Api/src/V1/Rest/GoodApl/GoodAplResource.php
@@ -181,10 +181,11 @@ class GoodAplResource extends AbstractResourceListener
      */
     public function patchList($data)
     {
-//        var_dump($data); exit;
-        if (!empty($data['fasade'])){
-            foreach ($data['fasade'] as $key => $goodAplId){
-                $this->entityManager->getConnection()->update('goods', ['fasade_ex' => Goods::FASADE_EX_TRANSFERRED], ['apl_id' => $goodAplId]);
+        if (is_object($data)){
+            if(!empty($data->fasade)){
+                foreach ($data->fasade as $key => $goodAplId){
+    //                $this->entityManager->getConnection()->update('goods', ['fasade_ex' => Goods::FASADE_EX_TRANSFERRED], ['apl_id' => $goodAplId]);
+                }                
             }
         }
         return new ApiProblem(405, 'The PATCH method has not been defined for collections');
