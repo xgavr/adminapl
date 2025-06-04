b7b5bff4eb2d23b82b04fb3979dabab5074f7f71
dsumi <dsumi@hpbook>
2025-06-04 12:15:39 +0300

    nt

--- module/Api/src/V1/Rest/GoodApl/GoodAplResource.php
+++ module/Api/src/V1/Rest/GoodApl/GoodAplResource.php
@@ -205,7 +205,7 @@ class GoodAplResource extends AbstractResourceListener
     public function patchList($data)
     {
         if (is_object($data)){
-            
+            var_dump($data); exit;
             switch ($data->fasade){
                 case Goods::FASADE_EX_NEW: $nextFasade = Goods::FASADE_EX_OEM; break;
                 case Goods::FASADE_EX_OEM: $nextFasade = Goods::FASADE_EX_IMG; break;
