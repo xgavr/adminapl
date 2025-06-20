a1b680dd2c340a25974b90ddea3ed17f929a8403
dsumi <dsumi@hpbook>
2025-06-20 11:09:18 +0300

    nt

--- module/Application/src/Repository/GoodsRepository.php
+++ module/Application/src/Repository/GoodsRepository.php
@@ -1973,7 +1973,7 @@ class GoodsRepository extends EntityRepository
                     ;
             }
             if (!empty($params['categoryId'])){
-                $queryBuilder->andWhere('g.groupSite = :category')
+                $queryBuilder->andWhere('gs.id = :category')
                     ->setParameter('category', $params['categoryId'])    
                     ;
             }
