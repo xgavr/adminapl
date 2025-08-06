c286aa16a93de2337e5097150d1777967af2373d
dsumi <dsumi@hpbook>
2025-08-06 06:57:09 +0300

    nt

--- module/Api/src/V1/Rest/ApiLanding/ApiLandingResource.php
+++ module/Api/src/V1/Rest/ApiLanding/ApiLandingResource.php
@@ -58,7 +58,8 @@ class ApiLandingResource extends AbstractResourceListener
 //            }
             
             $orderData = [
-                'mode' => ($data->mode) ? $data->mode:Order::MODE_LANDING,
+//                'mode' => ($data->mode) ? $data->mode:Order::MODE_LANDING,
+                'mode' => empty($data->goods) ? Order::MODE_VIN:Order::MODE_ORDER,
                 'name' => ($data->name) ?? null,
                 'info' => (empty($data->need)) ? null:$data->need,
                 'address' => (empty($data->address)) ? null:$data->address,
