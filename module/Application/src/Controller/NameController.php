ab75088bc8a4af89097d80d8a3050efc8ff9f02c
dsumi <dsumi@hpbook>
2025-10-15 09:08:53 +0300

    nt

--- module/Application/src/Controller/NameController.php
+++ module/Application/src/Controller/NameController.php
@@ -988,7 +988,7 @@ class NameController extends AbstractActionController
                 $result = $query->getOneOrNullResult(2);
                 return new JsonModel([
                     'id' => $tokenGroup->getId(),
-                    'categoryName' => $$groupSite->getFullName(),
+                    'categoryName' => $groupSite->getFullName(),
                     'row' => $result,
                 ]);
             }
