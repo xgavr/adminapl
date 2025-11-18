fb23bf23235cd1192dcf6f13a47474fb6a9ee72f
dsumi <dsumi@hpbook>
2025-11-18 13:45:06 +0300

    nt

--- module/Fasade/src/Repository/GroupSiteRepository.php
+++ module/Fasade/src/Repository/GroupSiteRepository.php
@@ -34,7 +34,7 @@ class GroupSiteRepository extends EntityRepository{
 
         $queryBuilder = $entityManager->createQueryBuilder();
 
-        $queryBuilder->select('gs.id, gs.code, gs.name, gs.sort, gs.level, gs.goodCount, gs.saleCount')
+        $queryBuilder->select('gs.id, gs.code, gs.name, gs.sort, gs.level, gs.goodCount, gs.saleCount, gs.description')
             ->addSelect('gs.status, identity(gs.siteGroup) as pid')
             ->addSelect('gs.hasChild, gs.fullName')
             ->from(GroupSite::class, 'gs')
