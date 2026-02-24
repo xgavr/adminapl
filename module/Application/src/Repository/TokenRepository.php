adab62e17e2b152a4de05644333f46192f3e339e
dsumi <dsumi@hpbook>
2026-02-24 14:42:12 +0300

    nt

--- module/Application/src/Repository/TokenRepository.php
+++ module/Application/src/Repository/TokenRepository.php
@@ -413,7 +413,7 @@ class TokenRepository  extends EntityRepository
             }            
         }
 
-//            var_dump($queryBuilder->getQuery()->getSQL()); exit;
+            var_dump($queryBuilder->getQuery()->getSQL()); exit;
         return $queryBuilder->getQuery();
     }            
     
