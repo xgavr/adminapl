d0a3ee494a4815ff4379234d3ea44241259dad4e
dsumi <dsumi@hpbook>
2026-03-31 12:34:53 +0300

    nt

--- module/Application/src/Repository/ProducerRepository.php
+++ module/Application/src/Repository/ProducerRepository.php
@@ -376,7 +376,7 @@ class ProducerRepository  extends EntityRepository{
         
         if (is_array($params)){
             if (isset($params['q'])){
-                $queryBuilder->where('p.name like :search or up.name like :search')
+                $queryBuilder->where('p.name like :search or up.name like :search or p.aplId = :search')
                     ->setParameter('search', '%' . $params['q'] . '%')
                         ;
             }
