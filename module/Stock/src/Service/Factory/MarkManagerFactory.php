6dc699a38e2735b44314f822626a87f5236e5d5a
dsumi <dsumi@hpbook>
2026-04-16 13:10:27 +0300

    nt

---
+++ module/Stock/src/Service/Factory/MarkManagerFactory.php
@@ -0,0 +1,26 @@
+<?php
+namespace Stock\Service\Factory;
+
+use Interop\Container\ContainerInterface;
+use Stock\Service\MarkManager;
+use Admin\Service\LogManager;
+use Admin\Service\AdminManager;
+
+/**
+ * This is the factory class for MarkManager service. The purpose of the factory
+ * is to instantiate the service and pass it dependencies (inject dependencies).
+ */
+class MarkManagerFactory
+{
+    /**
+     * This method creates the UserManager service and returns its instance. 
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {        
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        $logManager = $container->get(LogManager::class);
+        $adminManager = $container->get(AdminManager::class);
+                        
+        return new OtManager($entityManager, $logManager, $adminManager);
+    }
+}
