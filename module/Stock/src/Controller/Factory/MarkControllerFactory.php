9e17e5226008fe3287d1f7d44f4a5459d9114286
dsumi <dsumi@hpbook>
2026-04-16 13:10:50 +0300

    nt

---
+++ module/Stock/src/Controller/Factory/MarkControllerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Stock\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Stock\Controller\MarkController;
+use Stock\Service\MarkManager;
+
+
+/**
+ * Description of MarkControllerFactory
+ *
+ * @author Daddy
+ */
+class MarkControllerFactory implements FactoryInterface {
+    
+    public function __invoke(ContainerInterface $container, 
+                     $requestedName, array $options = null)
+    {
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        $markManager = $container->get(MarkManager::class);
+        
+        // Инстанцируем контроллер и внедряем зависимости.
+        return new OtController($entityManager, $markManager);
+    }
+}
