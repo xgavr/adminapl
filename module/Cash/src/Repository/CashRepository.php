a09ad3983e764bd6a59e87204e61ba91ee1d4b52
dsumi <dsumi@hpbook>
2026-09-05 09:06:15 +0300

    nt

--- module/Cash/src/Repository/CashRepository.php
+++ module/Cash/src/Repository/CashRepository.php
@@ -114,9 +114,15 @@ class CashRepository extends EntityRepository
         if (is_array($params)){
             if (!empty($params['cashId'])){
                 if (is_numeric($params['cashId'])){
-                    $queryBuilder->andWhere('ct.cash = ?3')
-                        ->setParameter('3', $params['cashId'])
-                            ;
+                    if ($params['cashId'] < 1000){
+                        $queryBuilder->andWhere('ct.cash = ?3')
+                            ->setParameter('3', $params['cashId'])
+                                ;
+                    } else {
+                        $queryBuilder->andWhere('cdc.bankInn = ?3')
+                            ->setParameter('3', $params['cashId'])
+                                ;                        
+                    }
                 }    
             }            
             if (!empty($params['office'])){
