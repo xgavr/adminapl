26b43aafa35acd6ccdd758be3918f95bcef3c27d
dsumi <dsumi@hpbook>
2026-09-17 15:12:35 +0300

    nt

--- module/Api/src/V1/Rest/ApiQrcode/ApiQrcodeResource.php
+++ module/Api/src/V1/Rest/ApiQrcode/ApiQrcodeResource.php
@@ -155,14 +155,52 @@ class ApiQrcodeResource extends AbstractResourceListener
                     'amount' => $toFloat->filter(round($params->amount/100, 2)),
                 ]);
                 
+//                if ($qrCode){
+//                    $clickFilter = new ClickFilter();
+//                    $result = $qrCode->toMsg();
+//                    $result['payloadShort'] = $clickFilter->filter($result['payload']);
+//                    return [
+//                        'qrcode' => $result,
+//                    ];
+//                }
+                
                 if ($qrCode){
                     $clickFilter = new ClickFilter();
                     $result = $qrCode->toMsg();
                     $result['payloadShort'] = $clickFilter->filter($result['payload']);
-                    return [
-                        'qrcode' => $result,
-                    ];
-                }
+                    
+                    //Добавить предоплаты
+                    $prepaymets = $this->getPrepaymentAmounts($params->amount);
+//                    var_dump($prepaymets); exit;
+                    
+                    if (!empty($prepaymets['prepayment_20'])){
+                        $qrCodeP20 = $this->sbpManager->registerQrCode([
+                            'orderAplId' => $params->order,
+                            'amount' => $toFloat->filter($prepaymets['prepayment_20']),
+                            'prepay' => true,
+                        ]);
+                        
+                        if ($qrCodeP20){
+                             $result['p20'] = $qrCodeP20->toMsg();
+                             $result['p20']['payloadShort'] = $clickFilter->filter($result['p20']['payload']);
+                        }                    
+                    } 
+                    
+                    if (!empty($prepaymets['prepayment_50'])){
+                        $qrCodeP50 = $this->sbpManager->registerQrCode([
+                            'orderAplId' => $params->order,
+                            'amount' => $toFloat->filter($prepaymets['prepayment_50']),
+                            'prepay' => true,
+                        ]);
+                        
+                        if ($qrCodeP50){
+                             $result['p50'] = $qrCodeP50->toMsg();
+                             $result['p50']['payloadShort'] = $clickFilter->filter($result['p50']['payload']);
+                        }                    
+                    }    
+                                        
+                    return $result;
+                }                
             }
         }
         return new ApiProblem(405, 'The GET method has not been defined for collections');
