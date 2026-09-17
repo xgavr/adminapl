8517a35c14a71e9b07a274183eb35ee87d94160e
dsumi <dsumi@hpbook>
2026-09-17 15:25:01 +0300

    nt

--- module/Api/src/V1/Rest/ApiQrcode/ApiQrcodeResource.php
+++ module/Api/src/V1/Rest/ApiQrcode/ApiQrcodeResource.php
@@ -152,7 +152,7 @@ class ApiQrcodeResource extends AbstractResourceListener
                 $toFloat = new ToFloat();
                 $qrCode = $this->sbpManager->registerQrCode([
                     'orderAplId' => $params->order,
-                    'amount' => $toFloat->filter(round($params->amount/100, 2)),
+                    'amount' => $toFloat->filter($params->amount),
                 ]);
                 
 //                if ($qrCode){
