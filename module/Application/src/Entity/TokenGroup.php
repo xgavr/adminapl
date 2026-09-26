438e5113404ffdc11b92042db9879eef68eccbe4
dsumi <dsumi@hpbook>
2026-09-26 13:08:13 +0300

    nt

--- module/Application/src/Entity/TokenGroup.php
+++ module/Application/src/Entity/TokenGroup.php
@@ -23,7 +23,7 @@ use Fasade\Entity\GroupSite;
 class TokenGroup {
     
     const FREQUENCY_MIN   = 5000; // минимальная чатота токена
-    const MIN_GOODCOUNT = 10; // минимальное количество товаров в группе
+    const MIN_GOODCOUNT = 1; // минимальное количество товаров в группе
 
     /**
      * @ORM\Id
